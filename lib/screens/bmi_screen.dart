8c405412f9a2e8decfdecb4d282e94e0ec1ab718
Leonardo Vaz <87207693+leowvazd@users.noreply.github.com>
2022-09-26 16:31:45 -0300

    working on BMI screen

--- lib/screens/bmi_screen.dart
+++ lib/screens/bmi_screen.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:global_fitness/shared/menu_bottom.dart';
 import 'package:global_fitness/shared/menu_drawer.dart';
 
-class BmiScreen extends StatelessWidget {
+class BmiScreen extends StatefulWidget {
   const BmiScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BmiScreen> createState() => _BmiScreenState();
+}
+
+class _BmiScreenState extends State<BmiScreen> {
+  final double fontSize = 18;
+  String result = '';
+  bool isMetric = true;
+  bool isImperial = false;
+  double? height;
+  double? weight;
+  late List<bool> isSelected;
+
+  @override
+  void initState() {
+    isSelected = [isImperial, isMetric];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +30,48 @@ class BmiScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('BMI Calculator')),
       bottomNavigationBar: const MenuBottom(),
       drawer: const MenuDrawer(),
-      body: const Center(
-        child: FlutterLogo(),
-      ),
+      body: Column(children: [
+        ToggleButtons(
+            isSelected: isSelected,
+            onPressed: toggleMeasure,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: Text(
+                  'Metric',
+                  style: TextStyle(
+                    fontSize: fontSize,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: Text(
+                  'Imperial',
+                  style: TextStyle(
+                    fontSize: fontSize,
+                  ),
+                ),
+              ),
+            ]),
+      ]),
     );
+  }
+
+  void toggleMeasure(value) {
+    if (value == 0) {
+      isMetric = true;
+      isImperial = false;
+    } else {
+      isMetric = false;
+      isImperial = true;
+    }
+    setState(() {
+      isSelected = [isMetric, isImperial];
+    });
   }
 }
