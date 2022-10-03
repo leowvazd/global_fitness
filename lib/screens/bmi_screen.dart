e06ab2a33a55084e7f9c88c7bd86298e0294e10d
Leonardo Vaz <87207693+leowvazd@users.noreply.github.com>
2022-10-03 13:34:30 -0300

    finished BMI Screen

--- lib/screens/bmi_screen.dart
+++ lib/screens/bmi_screen.dart
@@ -39,36 +39,47 @@ class _BmiScreenState extends State<BmiScreen> {
       appBar: AppBar(title: const Text('BMI Calculator')),
       bottomNavigationBar: const MenuBottom(),
       drawer: const MenuDrawer(),
-      body: Column(children: [
-        ToggleButtons(children: [
+      body: SingleChildScrollView(
+        child: Column(children: [
+          ToggleButtons(
+              isSelected: isSelected,
+              onPressed: toggleMeasure,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text('Metric', style: TextStyle(fontSize: fontSize)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text('Imperial', style: TextStyle(fontSize: fontSize)),
+                ),
+              ]),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Text('Metric', style: TextStyle(fontSize: fontSize)),
+            padding: const EdgeInsets.all(24.0),
+            child: TextField(
+              controller: txtHeight,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(hintText: heightMessage),
+            ),
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Text('Imperial', style: TextStyle(fontSize: fontSize)),
+            padding: const EdgeInsets.all(24.0),
+            child: TextField(
+              controller: txtWeight,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(hintText: weightMessage),
+            ),
           ),
-        ], isSelected: isSelected, onPressed: toggleMeasure),
-        TextField(
-          controller: txtHeight,
-          keyboardType: TextInputType.number,
-          decoration: InputDecoration(hintText: heightMessage),
-        ),
-        TextField(
-          controller: txtWeight,
-          keyboardType: TextInputType.number,
-          decoration: InputDecoration(hintText: weightMessage),
-        ),
-        ElevatedButton(
-          child: Text('Calculate BMI', style: TextStyle(fontSize: fontSize)),
-          onPressed: () {},
-        ),
-        Text(
-          result,
-          style: TextStyle(fontSize: fontSize),
-        )
-      ]),
+          ElevatedButton(
+            onPressed: findBMI,
+            child: Text('Calculate BMI', style: TextStyle(fontSize: fontSize)),
+          ),
+          Text(
+            result,
+            style: TextStyle(fontSize: fontSize),
+          )
+        ]),
+      ),
     );
   }
 
@@ -82,6 +93,22 @@ class _BmiScreenState extends State<BmiScreen> {
     }
     setState(() {
       isSelected = [isMetric, isImperial];
+    });
+  }
+
+  void findBMI() {
+    double bmi = 0;
+    double height = double.tryParse(txtHeight.text) ?? 0;
+    double weight = double.tryParse(txtWeight.text) ?? 0;
+
+    if (isMetric) {
+      bmi = weight / (height * height);
+    } else {
+      bmi = weight * 703 / (height * height);
+    }
+
+    setState(() {
+      result = 'Your BMi is ${bmi.toStringAsFixed(2)}';
     });
   }
 }
